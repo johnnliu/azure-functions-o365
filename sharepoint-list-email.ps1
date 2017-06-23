9a585418f1b63e8692ff542b38617512bb890be6
John Liu <johnnliu@users.noreply.github.com>
2017-06-23 22:50:04 +1000

    Update sharepoint-list-email.ps1

--- sharepoint-list-email.ps1
+++ sharepoint-list-email.ps1
@@ -1,30 +1,25 @@
 Import-Module "D:\home\site\wwwroot\modules\SharePointPnPPowerShellOnline.psd1" -Global;
 
-$requestBody = Get-Content $req -Raw | ConvertFrom-Json
-$name = $requestBody.name
+#$requestBody = Get-Content $req -Raw | ConvertFrom-Json
+#$name = $requestBody.name
 
+$siteUrl = "https://johnliu365.sharepoint.com"
 $username = "service@johnliu365.onmicrosoft.com";
 $password = $env:PW;
-$siteUrl = "https://johnliu365.sharepoint.com"
-
 $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
 $creds = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
 
 "try connect to $siteUrl"
 Connect-PnPOnline -url $siteUrl -Credentials $creds 
 
-$items = Get-PnPListItem -List "Documents"
-
 $mylist = @()
-
+$items = Get-PnPListItem -List "Documents"
 $items | % {
-
     $mylist += (new-object psobject -property @{
         File     = $_.FieldValues["FileLeafRef"]
         Editor   = $_.FieldValues["Editor"].LookupValue 
         Modified = $_.FieldValues["Last_x0020_Modified"]
     })
-
 }
 
 $mylist = $mylist | Sort-Object Modified -descending
@@ -33,4 +28,4 @@ $body = $body | out-string
 
 Send-PnPMail -To "john.liu@johnliu365.onmicrosoft.com" -Subject "list library" -Body $body
 
-Out-File -Encoding Ascii -FilePath $res -inputObject "Done $name" 
+#Out-File -Encoding Ascii -FilePath $res -inputObject "Done $name" 
